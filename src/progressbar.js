e1e9c9fea4550a8c6ad923ab10360a66740258b6
Matthew Mason <me@mtthwmsn.co.uk>
2018-10-05 19:05:49 +0100

    Extend JS
    allow for multiple progress bars

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,30 +1,44 @@
 (function() {
 	"use strict";
-	var lists;
 
 	function init() {
-		lists = Array.from(document.getElementsByClassName('progressbar'));
-		lists.forEach(ul => initList(ul));
+		let progressbar = Array.from(document.getElementsByClassName('progressbar'));
+		progressbar.forEach(bar => new Progressbar(bar));
 	}
 
-	function initList(list) {
-		let items = Array.from(list.getElementsByTagName("li"));
-		items.forEach((li, i) => {
-			li.className = li.className.concat(" progressbar__item").trim();
-			let content = li.innerHTML;
-			li.innerHTML = "";
+	function Progressbar(bar) {
+		var p = {};
+		p.activeIndex = 0;
+		p.bar = bar;
+		p.points = Array.from(p.bar.getElementsByTagName("li"));
+		p.points.forEach((point, i) => initPoint(point, i));
+		bindEvents();
+
+		function bindEvents() {
+			console.log()
+		}
+
+		function initPoint(point, index) {
+			// get first `active` point if provided
+			if (!!point.dataset.active && p.activeIndex === 0)
+				p.activeIndex = index;
+			else
+				delete point.dataset.active;
+			// prep each point
+			point.className = point.className.concat(" progressbar__item").trim();
+			let c = point.innerHTML;
+			point.innerHTML = "";
 			// construct step title
 			let s = document.createElement("DIV");
 			s.className = "progressbar__title";
-			s.innerHTML = (list.dataset.stepPrefix ? list.dataset.stepPrefix : "Step").trim();
-			s.innerHTML += " "+(i + 1);
-			li.append(s);
+			s.innerHTML = (p.bar.dataset.stepPrefix||"Step").trim().concat(" "+(index + 1));
+			point.append(s);
 			// construct wrapper for content
 			let w = document.createElement("DIV");
 			w.className = "progressbar__content";
-			w.append(content);
-			li.append(w);
-		});
+			w.innerHTML = c;
+			point.append(w);
+		}
 	}
 
 	init();
