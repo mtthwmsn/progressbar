3c213d8a73de5c2a54572f4d26c39c259f533f38
Matthew Mason <me@mtthwmsn.co.uk>
2018-10-05 16:46:39 +0100

    JS and simplify HTML
    make the JS do the work

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,1 +1,31 @@
-console.log("test");
+(function() {
+	"use strict";
+	var lists;
+
+	function init() {
+		lists = Array.from(document.getElementsByClassName('progressbar'));
+		lists.forEach(ul => initList(ul));
+	}
+
+	function initList(list) {
+		let items = Array.from(list.getElementsByTagName("li"));
+		items.forEach((li, i) => {
+			li.className = li.className.concat(" progressbar__item").trim();
+			let content = li.innerHTML;
+			li.innerHTML = "";
+			// construct step title
+			let s = document.createElement("DIV");
+			s.className = "progressbar__title";
+			s.innerHTML = (list.dataset.stepPrefix ? list.dataset.stepPrefix : "Step").trim();
+			s.innerHTML += " "+(i + 1);
+			li.append(s);
+			// construct wrapper for content
+			let w = document.createElement("DIV");
+			w.className = "progressbar__content";
+			w.append(content);
+			li.append(w);
+		});
+	}
+
+	init();
+})();
