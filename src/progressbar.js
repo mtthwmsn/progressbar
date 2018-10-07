3f4fba7dec924c33c36052cc5907465c46866720
Matthew Mason <me@mtthwmsn.co.uk>
2018-10-07 21:01:20 +0100

    Bind methods
    bind `next` and `prev` methods to progressbar

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,5 +1,5 @@
 (function() {
-	"use strict";
+	'use strict';
 
 	function init() {
 		let progressbar = Array.from(document.getElementsByClassName('progressbar'));
@@ -8,36 +8,54 @@
 
 	function Progressbar(bar) {
 		var p = {};
-		p.activeIndex = 0;
 		p.bar = bar;
-		p.points = Array.from(p.bar.getElementsByTagName("li"));
-		p.points.forEach((point, i) => initPoint(point, i));
+		p.points = null;
+		p.activePoint = null;
+
+		initPoints();
 		bindEvents();
 
 		function bindEvents() {
-			console.log()
+			// define methods
+			p.onNext = function() {
+				console.log('next...');
+			};
+			p.onPrev = function() {
+				console.log('prev...');
+			};
+			// bind methods to element
+			p.bar.next = p.onNext.bind(p.bar);
+			p.bar.prev = p.onPrev.bind(p.bar);
 		}
 
-		function initPoint(point, index) {
-			// get first `active` point if provided
-			if (!!point.dataset.active && p.activeIndex === 0)
-				p.activeIndex = index;
-			else
-				delete point.dataset.active;
-			// prep each point
-			point.className = point.className.concat(" progressbar__item").trim();
-			let c = point.innerHTML;
-			point.innerHTML = "";
-			// construct step title
-			let s = document.createElement("DIV");
-			s.className = "progressbar__title";
-			s.innerHTML = (p.bar.dataset.stepPrefix||"Step").trim().concat(" "+(index + 1));
-			point.append(s);
-			// construct wrapper for content
-			let w = document.createElement("DIV");
-			w.className = "progressbar__content";
-			w.innerHTML = c;
-			point.append(w);
+		function initPoints() {
+			let points = Array.from(p.bar.getElementsByTagName('li'));
+			points.forEach((point, i) => {
+				// define active index
+				if (point.dataset.active && p.activePoint === null)
+					p.activePoint = point;
+				else
+					delete point.dataset.active;
+
+				// prep each point
+				point.className = point.className.concat(' progressbar__item').trim();
+				let c = point.innerHTML;
+				point.innerHTML = '';
+				// construct step title
+				let s = document.createElement('DIV');
+				s.className = 'progressbar__title';
+				s.innerHTML = (p.bar.dataset.stepPrefix||'Step').trim().concat(' '+(i + 1));
+				point.append(s);
+				// construct wrapper for content
+				let w = document.createElement('DIV');
+				w.className = 'progressbar__content';
+				w.innerHTML = c;
+				point.append(w);
+			});
+
+			p.points = points;
+			p.activePoint = p.activePoint||p.points[0];
+			p.activePoint.dataset.active = 1;
 		}
 	}
 
